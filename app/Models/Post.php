2374b3aad8ae3a8b80d5c378c2265007a403a0a9
ramamoradi <ramamoradi@gmail.com>
2023-04-08 01:52:16 +0300

    test1

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function images(){
+        return $this->hasMany(Image::class,'post_id');
+
+    }
+    public function categories(){
+        return $this->hasMany(Category::class,'post_id');
+    }
+
+
+}

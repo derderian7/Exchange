3ecd96d8713acb166d04c8786d427b73971e22ea
tareemsa <tareemsasa21@gmail.com>
2023-05-19 18:20:52 +0300

    userprofile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,5 +9,32 @@ class UserController extends Controller
     //function to insert use posts in user profile 
     public function usersPost() {
         $posts = DB::table('posts')->where('user_id', auth()->id())->get();
+
+    }
+    public function EditUserProfile(){
+
+
+    }
+    public function update(Request $request,$id )
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|between:2,15',
+           
+           // ' profile image' => //later//
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $UserProfile=User::findorfail($id);
+        $UserProfile->update([
+            'name'=>$request->name,
+          //profile-imagelater
+        ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'UserProfile updated successfully',
+        ]);
+    
+    
     }
 }

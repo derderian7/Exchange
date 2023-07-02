6ba47461c56b79c40203ab9686bb8ac03450e5fc
derderian7 <georgederderian7@gmail.com>
2023-07-03 01:56:12 +0300

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,9 +7,6 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_get_posts()
     {
         $user=User::factory()->create();
@@ -19,25 +16,29 @@ class ExampleTest extends TestCase
     }
     public function test_get_GetAdmin()
     {
-        $response = $this->getJson('api/GetAdmin');
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/GetAdmin');
 
         $response->assertStatus(200);
     }
     public function test_get_show_report()
     {
-        $response = $this->getJson('api/show_report');
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/show_report');
 
         $response->assertStatus(200);
     }
     public function test_get_report_count()
     {
-        $response = $this->getJson('api/report_count');
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/report_count');
 
         $response->assertStatus(200);
     }
     public function test_get_CountMsg()
     {
-        $response = $this->getJson('api/CountMsg');
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/CountMsg');
 
         $response->assertStatus(200);
     }

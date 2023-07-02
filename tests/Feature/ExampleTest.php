ef0f24433beed8ab82a7f0fad12fbd56e230a524
derderian7 <georgederderian7@gmail.com>
2023-07-03 02:17:43 +0300

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_get_posts()
-    {
-        $user=User::factory()->create();
-        $response = $this->actingAs($user)->getJson('api/posts');
-
-        $response->assertStatus(200);
-    }
     public function test_get_GetAdmin()
     {
         $user=User::factory()->create();
@@ -39,6 +33,48 @@ class ExampleTest extends TestCase
     {
         $user=User::factory()->create();
         $response = $this->actingAs($user)->getJson('api/CountMsg');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_new_users()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/NewUsers');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_count_all_users()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/CountAllUsers');
+
+        $response->assertStatus(200);
+    } 
+    public function test_get_my_profile()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/getmyprofile');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_My_Rating()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/getMyRating');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_Wishlist()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/getWishlist');
+
+        $response->assertStatus(200);
+    }
+    public function test_get_percentage_of_locations()
+    {
+        $user=User::factory()->create();
+        $response = $this->actingAs($user)->getJson('api/percentage_of_locations');
 
         $response->assertStatus(200);
     }

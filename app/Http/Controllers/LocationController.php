7f6cc23a9fb7dfb8f7045a1c5b74f1ade9262ac5
ramamoradi <ramamoradi@gmail.com>
2023-07-02 18:26:09 +0300

    divide by zero

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -9,45 +9,51 @@ use Illuminate\Database\QueryException;
 
 class LocationController extends Controller
 {
-    public function percentage_of_locations()
-    {
-        try{
-        $locations = ["Aleppo",
-        "Damascus",
-        "Hama",
-        "Tartus",
-        "Latakia",
-        "Idlib",
-        "Homs",
-        "Deir Ez-Zor",
-        "Daraa ",
-        "As-Suwayda",
-        "Raqqa ",
-        "Quneitra",
-        "Al-Hasakah",
-        "Rif Dimashq"]; 
-        $result = [];
-
-        $totalPosts = Post::count();
-
-        foreach ($locations as $location) {
-            $locationPosts = Post::where('location', $location)->count();
-            $percentage = ($locationPosts / $totalPosts);
-            $roundedPercentage = round($percentage * 100, 2);
-
-            $result[$location] = $roundedPercentage;
-        }
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $result,
-        ]);
-    }catch(QueryException $e){
-        return response()->json($e,500);
-      }catch(Exception $e){
-        return response()->json($e,500);
+  public function percentage_of_locations()
+  {
+      try {
+          $locations = [
+              "Aleppo",
+              "Damascus",
+              "Hama",
+              "Tartus",
+              "Latakia",
+              "Idlib",
+              "Homs",
+              "Deir Ez-Zor",
+              "Daraa",
+              "As-Suwayda",
+              "Raqqa",
+              "Quneitra",
+              "Al-Hasakah",
+              "Rif Dimashq"
+          ]; 
+  
+          $result = [];
+          $totalPosts = Post::count();
+  
+          if ($totalPosts == 0) {
+              throw new Exception('No posts found.');
+          }
+  
+          foreach ($locations as $location) {
+              $locationPosts = Post::where('location', $location)->count();
+              $percentage = ($locationPosts / $totalPosts);
+              $roundedPercentage = round($percentage * 100, 2);
+  
+              $result[$location] = $roundedPercentage;
+          }
+  
+          return response()->json([
+              'status' => 'success',
+              'data' => $result,
+          ]);
+      } catch(QueryException $e) {
+          return response()->json($e, 500);
+      } catch(Exception $e) {
+          return response()->json(['error' => $e->getMessage()], 500);
       }
-    }
+  }
 
     public function Count_of_locations()
     {
